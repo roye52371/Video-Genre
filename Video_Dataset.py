d013cd26dc586d081909d7052ac2e34cd734a29e
roye52371 <roi52371@gmail.com>
2022-03-11 20:04:57 +0200

    add file that test small dataset by reading 120 frames from full video, take 2 minutes for 5 jenre to end

--- Video_Dataset.py
+++ Video_Dataset.py
@@ -506,9 +506,139 @@ class vid_dataset_with_path_name(Dataset):
     def __len__(self):  # return count of sample we have
         return len(self.videos_paths)
 
+class small_Dataset_for_show(Dataset):
+    def __init__(self, videos_path, classes_path, seq_size, resize_image=(180, 220),num_of_jenre=5):
+        # self.train_videos_paths_txt = glob.glob(os.path.join(train_videos_path, '*', '*.txt'))
+        # self.videos_paths = glob.glob(os.path.join(videos_path, '*','*','*.mp4'))# keep all frame video folder intervals paths
+        # self.videos_paths = self.videos_paths[0:8] # delete this line
+
+        # start comment: what to do when wanting to NOT take Jenres from our DATASETS
+        allvideosnotsureneeded = glob.glob(os.path.join(videos_path, '*', '*.mp4'))  # keep all frame video folder intervals paths
+        self.videos_paths = []
+        #print(num_of_jenre)
+        print("size dataset before changes:\n")
+        print(len(allvideosnotsureneeded))
+        for vid in allvideosnotsureneeded:
+            path = os.path.normpath(vid)
+            b = path.split(os.sep)
+            jenre = b[len(b) - 2]
+            #print(jenre)
+            if(num_of_jenre==5):
+                if ((jenre != "Animation") & (jenre != "Ice Hockey") & (jenre != "Judo") & (jenre != "Soccer")
+                        & (jenre != "Swimming(in Pool)") & (jenre != "Tennis") & (jenre != "Volleyball")
+                        & (jenre != "American Football") & (jenre != "Golf") & (jenre != "Graffiti Art")
+                        & (jenre != "Hair Style")):  # & (jenre != "Speeches and Lectures-Talks")
+                    # chosee jenres to take out
+                    # in if statement enter only if it is not one of the jenres you want out
+                    # afterwards, go to evert fivejenre_classes_with_HairStyle.txt files in your different datatsets
+                    # and delete it(Dataset, Dataset70_30,Dataset80_20)
+                    # than update in server, this(HP_dataset.py) file
+                    # and fivejenre_classes_with_HairStyle.txt files in all needed places in server code
+                    self.videos_paths.append(vid)
+            if(num_of_jenre==9):
+                if ((jenre != "Animation") & (jenre != "Ice Hockey") & (jenre != "Judo") & (jenre != "Soccer")
+                        & (jenre != "Swimming(in Pool)") & (jenre != "Tennis") & (jenre != "Volleyball")):
+                    # chosee jenres to take out
+                    # in if statement enter only if it is not one of the jenres you want out
+                    # afterwards, go to evert classes.txt files in your different datatsets
+                    # and delete it(Dataset, Dataset70_30,Dataset80_20)
+                    # than update in server, this(HP_dataset.py) file
+                    # and classes.txt files in all needed places in server code
+                    self.videos_paths.append(vid)
+            if(num_of_jenre==16):
+                self.videos_paths.append(vid)
+        print("size dataset after changes:\n")
+        print(len(self.videos_paths))
+        # print(self.videos_paths)
+
+        # end start commet: what to do when wanting to NOT take Jenres from our DATASETS
+
+        self.seq_size = seq_size  # should be according to frames created per video in offline proccesing
+        self.resize_image = resize_image
+        random.shuffle(self.videos_paths)
+
+        with open(classes_path, "r") as read_file:
+            self.class_num = json.load(read_file)  # check if this is the way to read the classes numbers
+
+        print("jenre size:\n")
+        print(len(self.class_num))
+        # this is the main method that we will use
+        # getitem is used let's use with array calls
+
+    def __getitem__(self, index):
+        # print(self.videos_path)
+
+        video_path = self.videos_paths[index]
+        # print(video_path)
+        # tensor = torch.zeros((len(data), 1, self.hand_points))
+        tensor = torch.zeros(self.seq_size, 3, self.resize_image[0], self.resize_image[1])
+
+        cap = cv2.VideoCapture(video_path)
+        # if(cap.isOpened()):
+        totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        # print(totalFrames)
+        frame_counter = 0
+        if (cap.isOpened()):
+            jumping_frames = int(np.floor(totalFrames / self.seq_size))  # need to take frame after this number of times
+            # frame_index_array = []
+            for i in range(0,self.seq_size):  # data size is the video size, check if start from 0 or 1 and end with size or size+1
+                # need to add index of frame  that devide with out reminder in self.seq_size from the specific video
+                # cap.set(cv2.CAP_PROP_POS_FRAMES,i*jumping_frames)
+                # changed to dumpy run over jumping frames, to prevent error accurs using cap.set in for loop
+                for j in range(0, jumping_frames - 1):
+                    ret, frame = cap.read()
+                ret, frame = cap.read()
+                if ret == True:
+                    resizearr = np.resize(frame, (3, 180, 220))
+                    tensor[i] = torch.from_numpy(resizearr)
+
+
+                else:
+
+                    print("frame didnt extracted well or finished if shows uup try to delete this printing, in video:\n")
+
+                    print(video_path)
+
+                    break
 
 
 
+        else:
+            print("cap could not open - in video:\n")
+            print(video_path)
+            exit()
+        cap.release()
+
+        # if(frame_counter != self.seq_size):
+        #     print("reading less than seq_size framesillegall interval size video\n")
+        #     print("in the video: ")
+        #     print(video_path)
+        #     print("our wanted seq size is: ")
+        #     print(self.seq_size)
+        #     print("while readed frames were: ")
+        #     print(frame_counter)
+        #     exit()
+
+        tensor = tensor / 255;  # normalize tensor
+        # print(tensor.size())
+
+        path = os.path.normpath(video_path)
+        b = path.split(os.sep)
+        # print(b)
+        jenre = b[len(b) - 2]
+        # print(jenre)
+        # b[len(b)-3] is the jenre I think
+        label = self.class_num[jenre]  # to check
+        # print(label)
+        # label = self.class_num[os.path.dirname(video_path).split('\\')[-1]]
+        # label = self.class_num[os.path.dirname(video_path).split('/')[-1]]
+        label = torch.tensor(label)
+        label = label.long()
+
+        return tensor, label
+
+    def __len__(self):  # return count of sample we have
+        return len(self.videos_paths)
 
 # if __name__ == '__main__':
 #      filename = 'Dataset70_30'  # OR "Dataset70_30"( to run second time with 'Dataset80_20')
---
+++ smallDataset_TestShow.py
@@ -0,0 +1,99 @@
+import cv2
+import numpy as np
+
+from torch.utils.data import DataLoader
+
+from Video_Dataset import fiveJenre_Dataset
+from Video_Dataset import nineJenre_Dataset
+from Video_Dataset import small_Dataset_for_show
+import torch
+#from HandLSTM import LSTMmodel
+from  LSTMmodel import LSTMmodel
+import torch.optim as optim
+import torch.nn as nn
+import os
+from Accuracy import calc_acc
+from create_confusion_matrix import calc_confusion_matrix
+#from EL.split_train_test import split
+from colorama import init
+from colorama import Fore, Back, Style
+from tqdm import tqdm
+from tqdm import trange
+
+
+def TestSmallDataset(Model_name,numOfJenre,classes_file): #model is str, numOfJenre is number, classes_file is txt file
+    filename = 'Dataset70_30' # OR "Dataset70_30"( to run second time with 'Dataset80_20')
+
+    init()
+
+    print("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    #hand_points = 42 * 3
+
+    #seq should be according to frames created per video in offline proccesing in convertedVideosToFrames.ipynb
+    seq=120#num of frames to take from one video, maybe to change , defend on what video we reading
+
+
+    test_path_videos = os.path.join(filename, 'test_show')
+    test_dataset = small_Dataset_for_show(test_path_videos, os.path.join(filename, classes_file), seq, (180, 220),numOfJenre)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=0)
+
+
+
+    #isBi = True # need to run one with tru and one with false
+    isBi = True
+    ####### LSTM Params #########
+    #output_size = 16
+    output_size = numOfJenre #delete problematic jenres which did not recognized by the model or confused to many other jenres
+    # input of lstm
+    #latent_dim = hand_points
+    latent_dim = 512# roye and dekel latent dim according to borak
+    # the vector length received from the past lstm run
+    hidden_dim = 256
+    # how many frames to look back (check it)
+    #n_layers = 2
+    #n_layers = 1 # roye and dekel num of lstm layers according to borak
+    n_layers=1
+    net = LSTMmodel(output_size, latent_dim, hidden_dim, n_layers, model_name='Basic', isBi=isBi)
+    # use the available device in the model
+    net = net.to(device)
+
+    #load model
+    net = net.to(device)
+    _use_new_zipfile_serialization = False
+    model_path = Model_name
+    net.load_state_dict(torch.load(f'{model_path}.pth'))
+
+
+
+    accurcy = 0
+    net.eval()
+    #test_data_counter = 0
+    #print("start test\n")
+    with tqdm(test_loader, unit="batch",leave=False,position=2) as test_epoch:
+        test_epoch.set_description(f"Test_Model with {numOfJenre} Jenres")#cause ep start from o and end in epoches-1
+        for hp_data, label in test_epoch:
+            #test_data_counter=test_data_counter+1
+            #print(str(curr_video_path[0]))
+            hp_data = hp_data.to(device)
+            label = label.to(device)
+            output = net(hp_data)
+            if torch.argmax(output) == label:
+                accurcy = accurcy + 1
+
+    our_accuracy = ((accurcy / len(test_loader)) * 100)
+    print("\n")
+    print("Model Name: ")
+    print(Model_name)
+    print("Num Of Jenre: ")
+    print(str(numOfJenre))
+    print("Model Accuracy: ")
+    print(str(our_accuracy))
+    print("\n")
+
+
+if __name__ == '__main__':
+    TestSmallDataset("NumOfJenre_5_model_CNN+LSTM_Dataset70_30_isBi:_True_accuracy=54.90196078431373",5,'fivejenre_classes.txt')
+    TestSmallDataset("CNN+LSTM_Dataset70_30_isBi:_True_accuracy=42.37472766884531", 9,'ninejenre_classes.txt')
+    TestSmallDataset("CNN+LSTM_Dataset70_30_isBi:_True_accuracy=24.019607843137255", 16,'sixteenjenre_classes.txt')
